33ee288f36c1557affabf3f22bf40feb8af81634
Felix Chan <felixchan29@gmail.com>
2018-06-24 21:19:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -51,6 +51,7 @@ function total() {
 
 function removeFromCart(item) {
   // write your code here
+  cart
   for(let i = 0; i < cart.length;i++){
     var isItem = cart[i].itemName;
     if(isItem === item){
