329403896bac96cfaf6ca3327c1c7304f273b7f9
Felix Chan <felixchan29@gmail.com>
2018-06-24 20:27:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ function viewCart() {
   }
   var cartList = `In your cart, you have ${cart[0].itemName} at ${cart[0].itemPrice}`;
   for(let i = 1; i < cart.length;i++){
-    cartList += `, ${cart[i].itemName} at ${cart[i].itemPrice}`;
+    cartList += `, ${cart[i].itemName} at $${cart[i].itemPrice}`;
   }
   cartList += `.`
   return cartList;
@@ -42,7 +42,7 @@ function removeFromCart(item) {
   for(let i = 0; i < cart.length;i++){
     if(item === cart[i].itemName){
       cart.splice(i);
-      return cart.viewCart();
+      return viewCart();
     }
   }
   return "That item is not in your cart.";
@@ -53,5 +53,5 @@ function placeOrder(cardNumber) {
   if(!cardNumber){
     return "Sorry, we don't have a credit card on file for you."
   }
-  return `Your total cost is ${cart.total}, which will be charged to the card ${cardNumber}.`
+  return `Your total cost is ${total()}, which will be charged to the card ${cardNumber}.`
 }
